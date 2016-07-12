7d2805891af222a14486e6b331ba9a67d070e2aa
Barroso Filho <barrosofilho.ti@gmail.com>
2016-07-12 02:47:43 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+function MTable($a)
+{
+    echo "<tr>";
+    for ($_ = 1; $_ <= 10; $_++) {
+        echo "<td>" . $_ . " x " . $a . " = " . ($a * $_) . " | </td>";
+    }
+    echo "</tr>";
+}
+
+echo "<table>";
+for ($_ = 1; $_ <= 10; $_++) {
+    MTable($_);
+}
+echo "</table>";
